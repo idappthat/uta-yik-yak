87e0973f8533c809fa1cb186aadb56d3e9fc3362
Zachery Gentry <zacherygentry@gmail.com>
2018-10-12 19:02:48 -0500

    hashed animal names 🦁🐅🐳

--- src/scenes/Splash.js
+++ src/scenes/Splash.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, AsyncStorage } from 'react-native';
 import yeet from '../../images/yeet520.png';
+import stringHash from "string-hash";
 import * as firebase from 'firebase';
 
 // Initialize Firebase
@@ -9,19 +10,270 @@ const firebaseConfig = {
   databaseURL: "https://yeet-54e58.firebaseio.com/"
 };
 
-function writeNewUser() {
+const animalNames = [
+  "Aardvark",
+  "Albatross",
+  "Alligator",
+  "Alpaca",
+  "Ant",
+  "Anteater",
+  "Antelope",
+  "Ape",
+  "Armadillo",
+  "Donkey",
+  "Baboon",
+  "Badger",
+  "Barracuda",
+  "Bat",
+  "Bear",
+  "Beaver",
+  "Bee",
+  "Bison",
+  "Boar",
+  "Buffalo",
+  "Butterfly",
+  "Camel",
+  "Capybara",
+  "Caribou",
+  "Cassowary",
+  "Cat",
+  "Caterpillar",
+  "Cattle",
+  "Chamois",
+  "Cheetah",
+  "Chicken",
+  "Chimpanzee",
+  "Chinchilla",
+  "Chough",
+  "Clam",
+  "Cobra",
+  "Cockroach",
+  "Cod",
+  "Cormorant",
+  "Coyote",
+  "Crab",
+  "Crane",
+  "Crocodile",
+  "Crow",
+  "Curlew",
+  "Deer",
+  "Dinosaur",
+  "Dog",
+  "Dogfish",
+  "Dolphin",
+  "Dotterel",
+  "Dove",
+  "Dragonfly",
+  "Duck",
+  "Dugong",
+  "Dunlin",
+  "Eagle",
+  "Echidna",
+  "Eel",
+  "Eland",
+  "Elephant",
+  "Elk",
+  "Emu",
+  "Falcon",
+  "Ferret",
+  "Finch",
+  "Fish",
+  "Flamingo",
+  "Fly",
+  "Fox",
+  "Frog",
+  "Gaur",
+  "Gazelle",
+  "Gerbil",
+  "Giraffe",
+  "Gnat",
+  "Gnu",
+  "Goat",
+  "Goldfinch",
+  "Goldfish",
+  "Goose",
+  "Gorilla",
+  "Goshawk",
+  "Grasshopper",
+  "Grouse",
+  "Guanaco",
+  "Gull",
+  "Hamster",
+  "Hare",
+  "Hawk",
+  "Hedgehog",
+  "Heron",
+  "Herring",
+  "Hippopotamus",
+  "Hornet",
+  "Horse",
+  "Human",
+  "Hummingbird",
+  "Hyena",
+  "Ibex",
+  "Ibis",
+  "Jackal",
+  "Jaguar",
+  "Jay",
+  "Jellyfish",
+  "Kangaroo",
+  "Kingfisher",
+  "Koala",
+  "Kookabura",
+  "Kouprey",
+  "Kudu",
+  "Lapwing",
+  "Lark",
+  "Lemur",
+  "Leopard",
+  "Lion",
+  "Llama",
+  "Lobster",
+  "Locust",
+  "Loris",
+  "Louse",
+  "Lyrebird",
+  "Magpie",
+  "Mallard",
+  "Manatee",
+  "Mandrill",
+  "Mantis",
+  "Marten",
+  "Meerkat",
+  "Mink",
+  "Mole",
+  "Mongoose",
+  "Monkey",
+  "Moose",
+  "Mosquito",
+  "Mouse",
+  "Mule",
+  "Narwhal",
+  "Newt",
+  "Nightingale",
+  "Octopus",
+  "Okapi",
+  "Opossum",
+  "Oryx",
+  "Ostrich",
+  "Otter",
+  "Owl",
+  "Oyster",
+  "Panther",
+  "Parrot",
+  "Partridge",
+  "Peafowl",
+  "Pelican",
+  "Penguin",
+  "Pheasant",
+  "Pig",
+  "Pigeon",
+  "Pony",
+  "Porcupine",
+  "Porpoise",
+  "Quail",
+  "Quelea",
+  "Quetzal",
+  "Rabbit",
+  "Raccoon",
+  "Rail",
+  "Ram",
+  "Rat",
+  "Raven",
+  "Red deer",
+  "Red panda",
+  "Reindeer",
+  "Rhinoceros",
+  "Rook",
+  "Salamander",
+  "Salmon",
+  "Sand Dollar",
+  "Sandpiper",
+  "Sardine",
+  "Scorpion",
+  "Seahorse",
+  "Seal",
+  "Shark",
+  "Sheep",
+  "Shrew",
+  "Skunk",
+  "Snail",
+  "Snake",
+  "Sparrow",
+  "Spider",
+  "Spoonbill",
+  "Squid",
+  "Squirrel",
+  "Starling",
+  "Stingray",
+  "Stinkbug",
+  "Stork",
+  "Swallow",
+  "Swan",
+  "Tapir",
+  "Tarsier",
+  "Termite",
+  "Tiger",
+  "Toad",
+  "Trout",
+  "Turkey",
+  "Turtle",
+  "Viper",
+  "Vulture",
+  "Wallaby",
+  "Walrus",
+  "Wasp",
+  "Weasel",
+  "Whale",
+  "Wildcat",
+  "Wolf",
+  "Wolverine",
+  "Wombat",
+  "Woodcock",
+  "Woodpecker",
+  "Worm",
+  "Wren",
+  "Yak",
+  "Zebra"
+];
+
+writeNewUser = async () => {
   let newUserKey = firebase.database().ref().child('users').push().key
 
-  // Write the new user data simultaneously in the users list
   var updates = {};
   updates['/users/' + newUserKey] = newUserKey;
+  _storeData(newUserKey);
 
   return firebase.database().ref().update(updates);
 }
 
-firebase.initializeApp(firebaseConfig);
-var database = firebase.database();
-writeNewUser();
+_storeData = async (value) => {
+  try {
+    await AsyncStorage.setItem('login', value);
+  } catch (error) {
+    // Error saving data
+  }
+}
+
+_hasKey = async () => {
+  try {
+    const value = await AsyncStorage.getItem('login');
+    if (value !== null) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  } catch (error) {
+    // Error retrieving data
+  }
+}
+
+getAnimalName = (value) => {
+  hashedValue = stringHash(value) % (animalNames.length - 1);
+  return animalNames[hashedValue];
+
+}
 
 
 class Splash extends Component {
@@ -37,13 +289,29 @@ class Splash extends Component {
     }
   }
 
+  componentWillMount = async () => {
+    //AsyncStorage.clear();
+    firebase.initializeApp(firebaseConfig);
+    const value = await AsyncStorage.getItem('login');
+    if (value == null) {
+      writeNewUser();
+    }
+    else {
+      this.setState(
+        {
+          name: getAnimalName(value)
+        }
+      );
+    }
+  }
+
   render() {
 
     return (
       <View style={styles.container}>
         <Image source={yeet} style={styles.image}></Image>
         <Text style={styles.text}>
-          YEET
+          {this.state.name}
         </Text>
         <View style={styles.button}>
           <Button
